1ff4eb87c82bfa563d98a1a784feee4011ab3e4e
Mariana <macbedoya@gmail.com>
2024-03-23 10:16:30 -0500

    Reactive forms - introduction

--- src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
+++ src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Publisher} from "../../interfaces/hero.interface";
 
 @Component({
   selector: 'app-add-new-hero-page',
@@ -6,11 +8,25 @@ import {Component} from '@angular/core';
   styleUrl: './add-new-hero-page.component.scss'
 })
 export class AddNewHeroPageComponent {
-  public publishers = [{
-    id: 'DC Comics',
-    desc: 'DC - Comics'
-  }, {
-    id: 'Marvel Comics',
-    desc: 'Marvel - Comics'
-  }];
+  public heroForm = new FormGroup({
+    id: new FormControl<string>('', {nonNullable: true}),
+    superhero: new FormControl<string>(''),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>(''),
+  });
+
+  public publishers = [
+    {id: 'DC Comics', desc: 'DC - Comics'},
+    {id: 'Marvel Comics', desc: 'Marvel - Comics'}
+  ];
+
+  onSubmit() {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 }
